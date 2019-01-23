c1ac5cbffe51a3e112ab721c9c72f2c1903b5908
gol <lorris.goyvaerts@ti8m.ch>
2019-01-23 15:57:35 +0100

    Added Permission Request for FINE Location

--- app/src/main/java/ch/ti8m/gol/bbw_route/geo/impl/LocationServiceImpl.kt
+++ app/src/main/java/ch/ti8m/gol/bbw_route/geo/impl/LocationServiceImpl.kt
@@ -1,23 +1,28 @@
 package ch.ti8m.gol.bbw_route.geo.impl
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.location.Location
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import ch.ti8m.gol.bbw_route.geo.LocationService
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
-class LocationServiceImpl(private val activity: Activity):LocationService {
+class LocationServiceImpl(private val activity: Activity) : LocationService {
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
 
     init {
-
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
     }
 
-
     override fun getLastKnownLocation(): Location {
         var temp = Location("TEMP")
-
     }
+
 }
--- app/src/main/java/ch/ti8m/gol/bbw_route/presentation/MainActivity.kt
+++ app/src/main/java/ch/ti8m/gol/bbw_route/presentation/MainActivity.kt
@@ -1,13 +1,89 @@
 package ch.ti8m.gol.bbw_route.presentation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import ch.ti8m.gol.bbw_route.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        // Here, activity is the current activity
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                Toast.makeText(this, "Permission is needed", Toast.LENGTH_SHORT).show()
+
+                // Request permission
+                startLocationPermissionRequest()
+
+            } else {
+                // No explanation needed, we can request the permission.
+                startLocationPermissionRequest()
+
+                // REQUEST_PERMISSIONS_REQUEST_CODE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+    }
+
+
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_PERMISSIONS_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSIONS_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 }
