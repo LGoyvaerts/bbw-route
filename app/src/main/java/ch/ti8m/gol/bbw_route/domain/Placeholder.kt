af0d63cec61f90619965681aad522643cb9fb20f
gol <lorris.goyvaerts@ti8m.ch>
2019-01-23 16:58:20 +0100

    Stop data class to translate JSON

---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/Placeholder.kt
@@ -0,0 +1,4 @@
+package ch.ti8m.gol.bbw_route.domain
+
+class Placeholder {
+}
---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/entity/Stop.kt
@@ -0,0 +1,11 @@
+package ch.ti8m.gol.bbw_route.domain.entity
+
+data class Stop(
+    val arrival: String,
+    val departure: String,
+    val name: String,
+    val stopid: String,
+    val x: Int,
+    val y: Int
+) {
+}
