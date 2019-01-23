83213f9fb1f06fc1bd50fef187895dee9a07fa09
gol <lorris.goyvaerts@ti8m.ch>
2019-01-23 15:25:47 +0100

    Location Service shall create FusedLocationProviderClient on init

--- app/src/main/java/ch/ti8m/gol/bbw_route/geo/impl/LocationServiceImpl.kt
+++ app/src/main/java/ch/ti8m/gol/bbw_route/geo/impl/LocationServiceImpl.kt
@@ -1,8 +1,15 @@
 package ch.ti8m.gol.bbw_route.geo.impl
 
-class LocationServiceImpl {
+import android.app.Activity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class LocationServiceImpl(private val activity: Activity) {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     init {
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
     }
 }
