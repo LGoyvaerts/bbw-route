700cbb482944424a628c1f75c6d3a97d845b207b
gol <lorris.goyvaerts@ti8m.ch>
2019-01-23 15:30:12 +0100

    LocationServiceImpl shall be derivated from its Interface

--- app/src/main/java/ch/ti8m/gol/bbw_route/geo/impl/LocationServiceImpl.kt
+++ app/src/main/java/ch/ti8m/gol/bbw_route/geo/impl/LocationServiceImpl.kt
@@ -1,15 +1,23 @@
 package ch.ti8m.gol.bbw_route.geo.impl
 
 import android.app.Activity
+import android.location.Location
+import ch.ti8m.gol.bbw_route.geo.LocationService
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
-class LocationServiceImpl(private val activity: Activity) {
+class LocationServiceImpl(private val activity: Activity):LocationService {
 
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     init {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
+    }
+
+
+    override fun getLastKnownLocation(): Location {
+        var temp = Location("TEMP")
+
     }
 }
