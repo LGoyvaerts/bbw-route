9b4292041bfeec728dba33deb737d9f280916f7c
gol <lorris.goyvaerts@ti8m.ch>
2019-01-24 11:58:32 +0100

    Section and Walk data class to translate JSON

---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/entity/Section.kt
@@ -0,0 +1,10 @@
+package ch.ti8m.gol.bbw_route.domain.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class Section(
+    @SerializedName("journey") val journey: Journey?,
+    @SerializedName("walk") val walk: Walk?,
+    @SerializedName("departure") val departure: Stop,
+    @SerializedName("arrival") val arrival: Stop
+)
---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/entity/Walk.kt
@@ -0,0 +1,3 @@
+package ch.ti8m.gol.bbw_route.domain.entity
+
+data class Walk(val duration: Long)
