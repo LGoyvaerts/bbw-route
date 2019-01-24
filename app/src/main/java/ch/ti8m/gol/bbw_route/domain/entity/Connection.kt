18da085200732ce280ebaea77beabb53eb558fca
gol <lorris.goyvaerts@ti8m.ch>
2019-01-24 12:09:08 +0100

    Connection data class to translate JSON

---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/entity/Connection.kt
@@ -0,0 +1,15 @@
+package ch.ti8m.gol.bbw_route.domain.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class Connection(
+    @SerializedName("from") val from: Stop?,
+    @SerializedName("to") val to: Stop?,
+    @SerializedName("duration") val duration: String,
+    @SerializedName("transfers") val transfers: Long,
+    @SerializedName("service") val service: Service?,
+    @SerializedName("products") val products: List<String>,
+    @SerializedName("capacity1st") val capacityFirst: Long?,
+    @SerializedName("capacity2nd") val capacitySecond: Long?,
+    @SerializedName("sections") val sections: List<Section>
+)
