fb354e2f705e8ed40f63cb8117d85bf9e0511f15
gol <lorris.goyvaerts@ti8m.ch>
2019-01-24 11:15:40 +0100

    Location data class to translate JSON

--- app/src/main/java/ch/ti8m/gol/bbw_route/domain/entity/Coordinate.kt
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/entity/Coordinate.kt
@@ -2,4 +2,4 @@ package ch.ti8m.gol.bbw_route.domain.entity
 
 import com.google.gson.annotations.SerializedName
 
-data class Coordinate(@SerializedName("type") val type: String, @SerializedName("x") val x: Long, @SerializedName("y") val y: Long)
+data class Coordinate(@SerializedName("type") val type: String, @SerializedName("x") val x: Double, @SerializedName("y") val y: Double)
