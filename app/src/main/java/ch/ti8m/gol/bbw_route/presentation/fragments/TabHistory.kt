0ed6aa4e0dd4d48eea87b0549cb4091580d208b6
gol <lorris.goyvaerts@ti8m.ch>
2019-01-24 17:05:05 +0100

    Added databinding to TabHistory

--- app/src/main/java/ch/ti8m/gol/bbw_route/presentation/fragments/TabHistory.kt
+++ app/src/main/java/ch/ti8m/gol/bbw_route/presentation/fragments/TabHistory.kt
@@ -3,9 +3,15 @@ package ch.ti8m.gol.bbw_route.presentation.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ch.ti8m.gol.bbw_route.databinding.FragmentHistoryBinding
 
 
 class TabHistory : Fragment() {
+
+    lateinit var binding: FragmentHistoryBinding
 
     companion object {
 
@@ -18,5 +24,14 @@ class TabHistory : Fragment() {
         }
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
+
+        //TODO init RecyclerView DataAdapter
+        //TODO initViews()
+
+        return binding.root
+    }
 
 }
