90828b149c90f5aa25201fcef67b45b9ca111ec1
gol <lorris.goyvaerts@ti8m.ch>
2019-01-25 13:17:50 +0100

    WeatherDataService to call GET on API

--- app/src/main/java/ch/ti8m/gol/bbw_route/remote/WeatherDataService.kt
+++ app/src/main/java/ch/ti8m/gol/bbw_route/remote/WeatherDataService.kt
@@ -1,13 +1,13 @@
 package ch.ti8m.gol.bbw_route.remote
 
+import ch.ti8m.gol.bbw_route.domain.entity.openweathermap.WeatherForecast
 import retrofit2.Call
 import retrofit2.http.GET
-import java.util.*
+import retrofit2.http.Path
 
 
 interface WeatherDataService {
 
-    //TODO
-    @GET("bins/1bsqcn/")
-    fun getNoticeData(): Call<Objects>
+    @GET("data/2.5/weather?lat={lat}&lon={lon}&appid=9c408d6546877c6e9872f13b7ddcb1ae")
+    fun getWeatherForecast(@Path("lat") lat: String, @Path("long") lon: String): Call<WeatherForecast>
 }
