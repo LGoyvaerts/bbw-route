67c010a414befb86267165a1c4ed1ecade39aae3
gol <lorris.goyvaerts@ti8m.ch>
2019-01-27 16:17:21 +0100

    ConnectionDetailActivity having its layout-file now created

---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/presentation/ConnectionDetailActivity.kt
@@ -0,0 +1,13 @@
+package ch.ti8m.gol.bbw_route.presentation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ch.ti8m.gol.bbw_route.R
+
+class ConnectionDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connection_detail)
+    }
+}
