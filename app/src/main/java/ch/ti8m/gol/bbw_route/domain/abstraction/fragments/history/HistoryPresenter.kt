27cea7af2e07da878291210b699f0a4077c75a99
gol <lorris.goyvaerts@ti8m.ch>
2019-01-29 13:33:34 +0100

    HistoryPresenter interface to use in Impl, keep UI away from domain, data etc.

---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/abstraction/fragments/history/HistoryPresenter.kt
@@ -0,0 +1,7 @@
+package ch.ti8m.gol.bbw_route.domain.abstraction.fragments.history
+
+interface HistoryPresenter {
+
+    fun initRepository()
+    fun loadSavedLocations()
+}
---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/domain/abstraction/fragments/history/HistoryPresenterImpl.kt
@@ -0,0 +1,4 @@
+package ch.ti8m.gol.bbw_route.domain.abstraction.fragments.history
+
+class HistoryPresenterImpl {
+}
