98461a4824352522ab6903949dc8be753dfd2904
gol <lorris.goyvaerts@ti8m.ch>
2019-01-29 10:03:08 +0100

    Creating DB and DAO/Repository now in onCreate() of App, to share between Activities

---
+++ app/src/main/java/ch/ti8m/gol/bbw_route/presentation/App.kt
@@ -0,0 +1,23 @@
+package ch.ti8m.gol.bbw_route.presentation
+
+import android.app.Application
+import ch.ti8m.gol.bbw_route.persistence.LocalSavedLocationRepository
+import ch.ti8m.gol.bbw_route.persistence.persistence.SavedLocationDAO
+import ch.ti8m.gol.bbw_route.persistence.persistence.SqlDatabase
+import ch.ti8m.gol.bbw_route.persistence.repository.SavedLocationRepository
+
+class App : Application() {
+
+    private lateinit var sqlDatabase: SqlDatabase
+    private lateinit var savedLocationDAO: SavedLocationDAO
+    private lateinit var savedLocationRepository: SavedLocationRepository
+
+    override fun onCreate() {
+        super.onCreate()
+
+        sqlDatabase = SqlDatabase(this)
+        savedLocationDAO = SavedLocationDAO(sqlDatabase)
+        savedLocationRepository = LocalSavedLocationRepository(savedLocationDAO)
+
+    }
+}
